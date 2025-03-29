106783b4cf482fef5f7f41bd6755ef92c36cd05a
Daniel Whiffing <danielwhiffing@gmail.com>
2025-03-29 19:16:17 -0400

    Uses motion

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 import { useMemo } from 'react'
 import { useElementSize } from '../hooks/useElementSize'
 import { Entity } from '../types'
