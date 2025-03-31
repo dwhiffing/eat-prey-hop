3f15778c954599a3edef710f2a56e4438da8c3f0
Daniel Whiffing <danielwhiffing@gmail.com>
2025-03-30 21:24:23 -0400

    Adds mobile controls

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -16,18 +16,34 @@ export function Game() {
       if (event.key === 'ArrowLeft') movePlayer(-1, 0)
       if (event.key === 'ArrowUp') movePlayer(0, -1)
       if (event.key === 'ArrowDown') movePlayer(0, 1)
-      if (event.key === ' ') movePlayer(0, 0)
     }, []),
   )
 
   return (
-    <div className="flex flex-col gap-2 items-center justify-center w-full h-full px-8">
+    <div className="flex flex-col gap-6 items-center justify-center w-full h-full px-8">
       <Grid gridSize={gridSize} maxWidth={maxWidth} />
       <div className="flex flex-row gap-5">
         <p className="text-white">Score: {score}</p>
         <p className="text-white">Multi: {getScoreMulti()}</p>
         <p className="text-white">Spawn: {spawnTimer}</p>
       </div>
+      <div className="flex flex-col items-center gap-1">
+        <MoveButton label="^" onClick={() => movePlayer(0, -1)} />
+        <div className="flex gap-1">
+          <MoveButton label="<" onClick={() => movePlayer(-1, 0)} />
+          <MoveButton label="v" onClick={() => movePlayer(0, 1)} />
+          <MoveButton label=">" onClick={() => movePlayer(1, 0)} />
+        </div>
+      </div>
     </div>
   )
 }
+
+const MoveButton = (props: { onClick: () => void; label: string }) => (
+  <button
+    className="bg-[#fff5] text-black aspect-square w-12"
+    onClick={props.onClick}
+  >
+    {props.label}
+  </button>
+)
